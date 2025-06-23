5fd5ff34e01816f15048ee0ee919053c548ab348
Kromaric <romaricyt11@gmail.com>
2025-06-23 04:07:55 +0200

    ajout de visiteur, de relation entre les table, ajout de la liste des guides, ajout du formulaire d'association de visit et visiteur

--- migrations/Version20250622195437.php
+++ migrations/Version20250622195437.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20250621184309 extends AbstractMigration
+final class Version20250622195437 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -21,7 +21,14 @@ final class Version20250621184309 extends AbstractMigration
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql(<<<'SQL'
+            CREATE TABLE associer (id INT AUTO_INCREMENT NOT NULL, visite_id INT NOT NULL, visiteur_id INT NOT NULL, presence TINYINT(1) NOT NULL, INDEX IDX_FA230DB9C1C5DC59 (visite_id), INDEX IDX_FA230DB97F72333D (visiteur_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB
+        SQL);
+        
+        $this->addSql(<<<'SQL'
             CREATE TABLE reset_password_request (id INT AUTO_INCREMENT NOT NULL, user_id INT NOT NULL, selector VARCHAR(20) NOT NULL, hashed_token VARCHAR(100) NOT NULL, requested_at DATETIME NOT NULL COMMENT '(DC2Type:datetime_immutable)', expires_at DATETIME NOT NULL COMMENT '(DC2Type:datetime_immutable)', INDEX IDX_7CE748AA76ED395 (user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB
+        SQL);
+        $this->addSql(<<<'SQL'
+            CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(180) NOT NULL, roles JSON NOT NULL, password VARCHAR(255) NOT NULL, first_name VARCHAR(255) NOT NULL, last_name VARCHAR(255) NOT NULL, profil VARCHAR(255) DEFAULT NULL, status TINYINT(1) NOT NULL, UNIQUE INDEX UNIQ_IDENTIFIER_EMAIL (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB
         SQL);
         $this->addSql(<<<'SQL'
             CREATE TABLE visit (id INT AUTO_INCREMENT NOT NULL, guide_id INT DEFAULT NULL, pictures JSON DEFAULT NULL, country VARCHAR(255) DEFAULT NULL, adress VARCHAR(255) DEFAULT NULL, date DATE DEFAULT NULL, start_time TIME DEFAULT NULL, end_time TIME DEFAULT NULL, duration VARCHAR(255) DEFAULT NULL, comment LONGTEXT DEFAULT NULL, INDEX IDX_437EE939D7ED1D4B (guide_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB
@@ -33,16 +40,29 @@ final class Version20250621184309 extends AbstractMigration
             CREATE TABLE messenger_messages (id BIGINT AUTO_INCREMENT NOT NULL, body LONGTEXT NOT NULL, headers LONGTEXT NOT NULL, queue_name VARCHAR(190) NOT NULL, created_at DATETIME NOT NULL COMMENT '(DC2Type:datetime_immutable)', available_at DATETIME NOT NULL COMMENT '(DC2Type:datetime_immutable)', delivered_at DATETIME DEFAULT NULL COMMENT '(DC2Type:datetime_immutable)', INDEX IDX_75EA56E0FB7336F0 (queue_name), INDEX IDX_75EA56E0E3BD61CE (available_at), INDEX IDX_75EA56E016BA31DB (delivered_at), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB
         SQL);
         $this->addSql(<<<'SQL'
+            ALTER TABLE associer ADD CONSTRAINT FK_FA230DB9C1C5DC59 FOREIGN KEY (visite_id) REFERENCES visit (id)
+        SQL);
+        $this->addSql(<<<'SQL'
+            ALTER TABLE associer ADD CONSTRAINT FK_FA230DB97F72333D FOREIGN KEY (visiteur_id) REFERENCES visiteur (id)
+        SQL);
+        $this->addSql(<<<'SQL'
             ALTER TABLE reset_password_request ADD CONSTRAINT FK_7CE748AA76ED395 FOREIGN KEY (user_id) REFERENCES user (id)
         SQL);
         $this->addSql(<<<'SQL'
             ALTER TABLE visit ADD CONSTRAINT FK_437EE939D7ED1D4B FOREIGN KEY (guide_id) REFERENCES user (id)
         SQL);
+        
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql(<<<'SQL'
+            ALTER TABLE associer DROP FOREIGN KEY FK_FA230DB9C1C5DC59
+        SQL);
+        $this->addSql(<<<'SQL'
+            ALTER TABLE associer DROP FOREIGN KEY FK_FA230DB97F72333D
+        SQL);
         $this->addSql(<<<'SQL'
             ALTER TABLE reset_password_request DROP FOREIGN KEY FK_7CE748AA76ED395
         SQL);
@@ -50,7 +70,19 @@ final class Version20250621184309 extends AbstractMigration
             ALTER TABLE visit DROP FOREIGN KEY FK_437EE939D7ED1D4B
         SQL);
         $this->addSql(<<<'SQL'
+            ALTER TABLE visit DROP FOREIGN KEY FK_437EE9392E0906D1
+        SQL);
+        $this->addSql(<<<'SQL'
+            DROP TABLE associer
+        SQL);
+        $this->addSql(<<<'SQL'
+            DROP TABLE guide_touristique
+        SQL);
+        $this->addSql(<<<'SQL'
             DROP TABLE reset_password_request
+        SQL);
+        $this->addSql(<<<'SQL'
+            DROP TABLE user
         SQL);
         $this->addSql(<<<'SQL'
             DROP TABLE visit
